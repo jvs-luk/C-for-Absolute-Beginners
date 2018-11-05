0853a3ec78f51e185d6453d789c438599518417b
Jessica Luk <jvs.luk@gmail.com>
2018-11-05 09:59:44 +0100

    Lesson 10

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+            
+        // Most simple method example
+        // NOTE how it's outside of void Main
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
