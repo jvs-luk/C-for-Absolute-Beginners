330382d3702d50ad05ad62fb1cee78c4f0723079
Jessica Luk <jvs.luk@gmail.com>
2018-11-05 11:34:49 +0100

    Lesson 11 added + Improved file organization

---
+++ 11- While Iteration Statement/11- While Iteration Statement/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11__While_Iteration_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+        private static void MainMenu()
+        {
+            Console.WriteLine("Choose one option:");
+            Console.WriteLine("1) Option 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Option 3");
+            Console.WriteLine("4) Exit");
+
+            string Result = Console.ReadLine();
+
+            if (Result == "1")
+            {
+
+            }
+
+            else if (Result == "2")
+            {
+
+            }
+
+            else if (Result == "3")
+            {
+
+            }
+
+            else if (Result == "4")
+            {
+
+            }
+
+
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print Numbers!");
+            Console.ReadLine();
+        }
+
+        private static void GuessingGame()
+        {
+            Console.WriteLine("Guessing game!");
+            Console.ReadLine();
+        }
+
+    }
+}
